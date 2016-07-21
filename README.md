18d86b1097e61472e1e0c913f67cbf4e33dd86f6
nuintun <nuintun@qq.com>
2016-07-21 15:25:18 +0800

    update files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# viewport
-A library for get a callback when any element becomes visible in a viewport (window or custom viewport)
+viewport
+=========
+
+>A library for get a callback when any element becomes visible in a viewport (window or custom viewport)
---
+++ examples/viewport.js
@@ -0,0 +1,25 @@
+define("viewport", ["jquery"], function(require, exports, module){
+/**
+ * Created by nuintun on 2016/7/21.
+ */
+/*!
+ * viewport
+ * Version: 0.0.1
+ * Date: 2016/7/21
+ * https://github.com/Nuintun/viewport
+ *
+ * Original Author: http://www.jsbug.com/lab/samples/viewport/
+ *
+ * This is licensed under the MIT License (MIT).
+ * For details, see: https://github.com/Nuintun/viewport/blob/master/LICENSE
+ */
+
+'use strict';
+
+var $ = require('jquery');
+
+function Viewport(elements, options){
+
+}
+
+});
