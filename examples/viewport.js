4999fdd6b6748418f928f384cf0dd8a77f429701
nuintun <nuintun@qq.com>
2016-07-21 16:49:06 +0800

    update files

--- examples/viewport.js
+++ examples/viewport.js
@@ -1,7 +1,4 @@
-define("viewport", ["jquery"], function(require, exports, module){
-/**
- * Created by nuintun on 2016/7/21.
- */
+define("viewport", ["jquery","./is"], function(require, exports, module){
 /*!
  * viewport
  * Version: 0.0.1
@@ -17,9 +14,115 @@ define("viewport", ["jquery"], function(require, exports, module){
 'use strict';
 
 var $ = require('jquery');
+var is = require('./is');
 
-function Viewport(elements, options){
+var expando = 0;
+var reference = {};
+var AP = Array.prototype;
 
+// ES5 15.4.4.20
+// https://developer.mozilla.org/en/Core_JavaScript_1.5_Reference/Objects/Array/filter
+AP.filter || (AP.filter = function (fn, context){
+  var result = [], val;
+
+  for (var i = 0, len = this.length >>> 0; i < len; i++) {
+    if (i in this) {
+      val = this[i]; // in case fn mutates this
+
+      if (fn.call(context, val, i, this)) {
+        result.push(val);
+      }
+    }
+  }
+
+  return result;
+});
+
+/**
+ * patch the threshold like css
+ * - 1            ==> top: 1, right: 1, bottom: 1, left: 1
+ * - [1]          ==> top: 1, right: 1, bottom: 1, left: 1
+ * - [1, 2]       ==> top: 1, right: 2, bottom: 1, left: 2
+ * - [1, 2, 3]    ==> top: 1, right: 2, bottom: 3, left: 2
+ * - [1, 2, 3, 4] ==> top: 1, right: 2, bottom: 3, left: 4
+ * @param threshold
+ * @returns {*}
+ */
+function patchThreshold(threshold){
+  if (is.number(threshold)) {
+    threshold = [threshold, threshold, threshold, threshold];
+  } else if (is.array(threshold)) {
+    threshold = threshold.filter(function (value){
+      return is.number(value);
+    });
+
+    switch (threshold.length) {
+      case 0:
+        threshold = [0, 0, 0, 0];
+        break;
+      case 1:
+        threshold = [threshold[0], threshold[0], threshold[0], threshold[0]];
+        break;
+      case 2:
+        threshold = [threshold[0], threshold[1], threshold[0], threshold[1]];
+        break;
+      case 3:
+        threshold = [threshold[0], threshold[1], threshold[2], threshold[1]];
+        break;
+      default:
+        threshold = threshold.slice(0, 4);
+        break;
+    }
+  } else {
+    threshold = [0, 0, 0, 0];
+  }
+
+  return threshold;
 }
+
+/**
+ * patch the viewport if it is the window object
+ * @param container
+ * @returns {*}
+ */
+function patchViewport(container){
+  return container === window
+    ? (document.compatMode == 'CSS1Compat' ? document.documentElement : document.body)
+    : container;
+}
+
+function Viewport(viewport, options){
+  this.id = expando++;
+  this.viewport = $(patchViewport(viewport));
+  this.options = $.extend({
+    delay: 150,
+    target: null,
+    threshold: 0,
+    includeHiddens: false,
+    thresholdBorderReaching: 0
+  }, options);
+
+  reference[this.id] = this;
+}
+
+Viewport.prototype = {
+  __init: function (){
+
+  },
+  __filterTargetInViewport: function (){
+
+  },
+  __findTarget: function (){
+    this.target = this.options.target
+      ? this.viewport.find(this.options.target)
+      : null;
+  },
+  refresh: function (){
+
+  },
+  destroy: function (){
+    delete reference[this.id];
+  }
+};
 
 });
