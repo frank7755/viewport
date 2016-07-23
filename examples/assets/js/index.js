88e404615000f3b201ac8c1f4a8ece45190d6d54
nuintun <nuintun@qq.com>
2016-07-23 17:36:25 +0800

    update files

--- examples/assets/js/index.js
+++ examples/assets/js/index.js
@@ -6,9 +6,22 @@ var $ = require('./jquery');
 var Viewport = require('./viewport');
 
 $(function (){
+  var info = {};
+  var nav = $('#nav');
+  var navPlaceholder = nav.clone().removeAttr('id').empty();
   var viewport = new Viewport(window, { target: '.ui-body img[data-src]' });
 
+  nav.find('span[data-info]').each(function (){
+    var element = $(this);
+
+    info[element.attr('data-info')] = element;
+  });
+
   viewport.on('viewchange', function (e){
+    for (var key in info) {
+      info[key].text(e[key]);
+    }
+
     $.each(e.target, function (i, element){
       element = $(element);
 
@@ -32,4 +45,23 @@ $(function (){
       }).attr('src', src);
     });
   });
+
+  var win = $(window);
+
+  function navFixed(){
+    var scrollTop = win.scrollTop();
+    var fixed = nav.hasClass('ui-nav-fixed');
+
+    if (scrollTop >= 20 && !fixed) {
+      nav.addClass('ui-nav-fixed');
+      nav.after(navPlaceholder);
+    } else if (scrollTop < 20 && fixed) {
+      navPlaceholder.remove();
+      nav.removeClass('ui-nav-fixed');
+    }
+  }
+
+  win.on('scroll', navFixed);
+
+  navFixed();
 });
