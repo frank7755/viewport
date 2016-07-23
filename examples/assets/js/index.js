c6e69c5282a0a869e9609a475571cf92640433a6
nuintun <nuintun@qq.com>
2016-07-23 16:31:58 +0800

    update files

--- examples/assets/js/index.js
+++ examples/assets/js/index.js
@@ -6,5 +6,30 @@ var $ = require('./jquery');
 var Viewport = require('./viewport');
 
 $(function (){
+  var viewport = new Viewport(window, { target: '.ui-body img[data-src]' });
 
+  viewport.on('viewchange', function (e){
+    $.each(e.target, function (i, element){
+      element = $(element);
+
+      var src = element.attr('data-src');
+
+      if (!src) return;
+
+      element.removeAttr('data-src');
+      element.addClass('ui-loading');
+
+      $('<img />').bind('load error', { image: element, src: src }, function (event){
+        var image = event.data.image;
+        var src = event.data.src;
+
+        element.removeClass('ui-loading');
+
+        image
+          .hide()
+          .attr('src', src)
+          .fadeIn('fast');
+      }).attr('src', src);
+    });
+  });
 });
